cdd41a500066339e935f4cf0585141030baed53c
Prank Shadow <prankshadow@gmail.com>
2024-05-31 13:56:37 +0530

    feat: added buttons explor

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -3,10 +3,11 @@ import { Navbar } from "@/components/navbar";
 import Image from "next/image";
 import stamp from "../assets/stamp.png";
 import { Button } from "./ui/button";
+import RightArrow from "../assets/right-arrow.svg";
 
 export default function HomeScreen() {
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-screen  sm: px-4">
+    <div className="flex flex-col gap-6 items-center justify-center h-screen sm: px-4">
       <Image src={stamp} alt="stamp" width={150} height={150} />
       <h2
         className={`${cormorantGaramond.className} text-6xl sm:text-7xl md:text-8xl text-center`}
@@ -14,11 +15,9 @@ export default function HomeScreen() {
         {`last24hr: Your Daily News Source`}
       </h2>
       <p className="text-2xl sm:text-3xl md:text-4xl sm: px-4 text-slate-500 text-center">{`"Read the Headlines, Explore the Full Story"`}</p>
-      <Button className="w-32 h-12 my-2 text-base">Read</Button>
-      {/* <Button className="w-32 h-12 my-2 bg-gray-800 text-white font-bold py-2 px-4 rounded relative overflow-hidden group">
-        <span className="absolute inset-0 bg-gradient-to-r from-transparent to-gray-300 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
-        <span className="relative">Explore</span>
-      </Button> */}
+      <Button className="h-12 px-5 my-2 text-base">
+        Start Exploring&nbsp;&nbsp;&nbsp;&nbsp;&#10140;
+      </Button>
     </div>
   );
 }
