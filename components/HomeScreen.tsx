613af8e313f0136c0943bb89826fb9b739fc8cf1
Prank Shadow <prankshadow@gmail.com>
2024-05-31 15:09:23 +0530

    created card

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -3,21 +3,58 @@ import { Navbar } from "@/components/navbar";
 import Image from "next/image";
 import stamp from "../assets/stamp.png";
 import { Button } from "./ui/button";
-import RightArrow from "../assets/right-arrow.svg";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function HomeScreen() {
   return (
-    <div className="flex flex-col gap-6 items-center sm: px-4">
+    <div className="flex flex-col gap-6 items-center sm:px-4">
       <Image src={stamp} alt="stamp" width={150} height={150} />
       <h2
         className={`${cormorantGaramond.className} text-6xl sm:text-7xl md:text-8xl text-center`}
       >
         {`last24hr: Your Daily News Source`}
       </h2>
-      <p className="text-2xl sm:text-3xl md:text-4xl sm: px-4 text-slate-500 text-center">{`"Read the Headlines, Explore the Full Story"`}</p>
-      <Button className="h-12 px-5 my-2 text-base">
-        Start Exploring&nbsp;&nbsp;&nbsp;&nbsp;&#10140;
-      </Button>
+      <p className="text-2xl sm:text-3xl md:text-4xl sm:px-4 text-slate-500 text-center">
+        {`"Read the Headlines, Explore the Full Story"`}
+      </p>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="h-12 px-5 my-2 text-base">
+            Start Exploring&nbsp;&nbsp;&nbsp;&nbsp;&#10140;
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="p-8 md:max-w-2xl gap-6">
+          <Image
+            src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="news-image"
+            width={600}
+            height={400}
+            className="rounded-md"
+          />
+          <DialogHeader className="gap-4">
+            <DialogTitle className="text-3xl">
+              Breakthrough in Quantum Computing: Scientists Achieve Stable Qubit
+              State at Room Temperature
+            </DialogTitle>
+            <DialogDescription className="text-xl">
+              In a monumental advancement for quantum computing, researchers at
+              the Quantum Computing Institute have successfully maintained a
+              stable qubit state at room temperature. This breakthrough could
+              significantly accelerate the development and practical application
+              of quantum computers,
+            </DialogDescription>
+            <Button>Read More</Button>
+            <p className="text-gray-500 text-right">Source: News 18</p>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
